65fbe0fed7e75fbac80ef18134396bf7ebee2a02
sloppie <dgitau75@gmail.com>
2021-03-16 22:53:09 +0300

    [UPDATED]: the scrapper now works sequentially all through without skippng a step in execution.
    [CONFIG]: added .gitignore file for the data-scrapper

--- data-scrapper/scrapper.py
+++ data-scrapper/scrapper.py
@@ -1,19 +1,16 @@
-from selenium.webdriver import Firefox, FirefoxOptions, ChromeOptions, Chrome
+from selenium.webdriver import ChromeOptions, Chrome
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-import selenium.webdriver.support.expected_conditions as Conditions
+import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 import re
 
 PATH = "C:\\Program Files (x86)\\web-drivers\\chrome\\chromedriver.exe"
-firefox_options = FirefoxOptions()
 chrome_options = ChromeOptions()
 chrome_options.add_argument("--incognito")
-# firefox_options.add_argument("--private")  # turn on incognito to prevent interfering with the User's search history
 
 driver = Chrome(executable_path=PATH, options=chrome_options)
-# driver = Firefox(executable_path=PATH, options=firefox_options)
 
 
 def search_for_item(web_driver, value):
@@ -21,27 +18,31 @@ def search_for_item(web_driver, value):
         web_driver.get("https://duckduckgo.com")
         # get the search form of the duckduckgo search engine
         search_form = WebDriverWait(web_driver, timeout=5).until(
-            lambda d: web_driver.find_element_by_id("search_form_input_homepage"))
+            lambda d: web_driver.find_element_by_id("search_form_input_homepage")
+            # lambda d: EC.element_to_be_clickable(By.ID, "search_form_input_homepage")
+        )
         search_form.click()
         search_form.send_keys(value)  # enter search
         search_form.send_keys(Keys.RETURN)
+
         return True
     except Exception as ex:
         print(ex)
+
         return False
 
 
 def turn_off_search_moderation(web_driver):
     try:
-        filter_menu = WebDriverWait(web_driver, timeout=10).until(
+        filter_menu = WebDriverWait(web_driver, timeout=15).until(
             lambda d: web_driver.find_element_by_xpath(
                 "/html/body/div[2]/div[5]/div[3]/div/div[1]/div[1]/div/div[2]/a"))
         filter_menu.click()
-        off_option = WebDriverWait(web_driver, timeout=10).until(
+        off_option = WebDriverWait(web_driver, timeout=15).until(
             lambda d: web_driver.find_element_by_xpath("/html/body/div[6]/div[2]/div/div/ol/li[3]/a")
         )
         off_option.click()
-        res = WebDriverWait(web_driver, timeout=10).until(
+        res = WebDriverWait(web_driver, timeout=15).until(
             lambda d: web_driver.find_element_by_xpath(
                 "/html/body/div[2]/div[5]/div[3]/div/div[1]/div[1]/div/div[2]/a").text == "Safe Search: Off"
         )
@@ -49,75 +50,87 @@ def turn_off_search_moderation(web_driver):
             print("Safe search Moderation off")
         while not res:
             print("Retrying safe search turn off")
+
         return True
     except Exception as ex:
         print(ex)
+
         return False
 
 
 def select_image_tab(web_driver):
     try:
-        duckbar = WebDriverWait(web_driver, timeout=10).until(
+        duckbar = WebDriverWait(web_driver, timeout=15).until(
             lambda d: web_driver.find_element_by_id("duckbar_static")
         )
-        items = WebDriverWait(duckbar, timeout=10).until(
+        items = WebDriverWait(duckbar, timeout=15).until(
             lambda d: duckbar.find_elements_by_class_name("zcm__link")
         )
+        time.sleep(1)
         for list_item in items:
             if list_item.text == "Images":
+                list_item
                 list_item.click()
                 break
+
         return True
     except Exception as ex:
         print(ex)
+
         return False
 
 
 def set_image_size(web_driver):
     try:
-        size_dropdown = WebDriverWait(web_driver, timeout=10).until(
-            lambda d: web_driver.find_element_by_class_name("dropdown--size")
+        size_dropdown = WebDriverWait(web_driver, timeout=15).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "dropdown--size"))
         )
         size_dropdown.click()  # open modal dropdown
         time.sleep(2)
-        modal_dropdown = WebDriverWait(web_driver, timeout=10).until(
+        modal_dropdown = WebDriverWait(web_driver, timeout=15).until(
             lambda d: web_driver.find_element_by_class_name("modal__list")
         )
-        anchor_elements = WebDriverWait(modal_dropdown, timeout=10).until(
+        anchor_elements = WebDriverWait(modal_dropdown, timeout=15).until(
             lambda d: modal_dropdown.find_elements_by_tag_name("a")
         )
-        for i, anchor_element in enumerate(anchor_elements):
+        for anchor_element in anchor_elements:
             if anchor_element.text == "Medium":
                 print("Size of photos set to medium")
                 anchor_element.click()
                 break
+        # This sleep is necessary since the window needs to refresh to update images with the new
+        # changes that come with changing the flag in question
+        time.sleep(2)
+
         return True
     except Exception as ex:
         print(ex)
+
         return False
 
 
 def set_image_type(web_driver):
     try:
-        type_dropdown = WebDriverWait(web_driver, timeout=10).until(
-            lambda d: web_driver.find_element_by_class_name("dropdown--type")
+        type_dropdown = WebDriverWait(web_driver, timeout=15).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "dropdown--type"))
         )
         type_dropdown.click()  # open the modal
-        time.sleep(2)
-        modal_container = WebDriverWait(web_driver, timeout=10).until(
-            lambda d: web_driver.find_element_by_class_name("modal__list")
-        )
+        time.sleep(1)
         anchor_elements = web_driver.find_elements_by_class_name("js-dropdown-items")
 
         for anchor_element in anchor_elements:
-            print(f"Media Type: {anchor_element.text}")
             if anchor_element.text == "Photograph":
                 print("Type of image set to: Photograph")
                 anchor_element.click()
                 break
+        # This sleep is necessary since the window needs to refresh to update images with the new
+        # changes that come with changing the flag in question
+        time.sleep(2)
+
         return True
     except Exception as ex:
         print(ex)
+
         return False  # signal that it was unable to select the image type
 
 
@@ -131,9 +144,11 @@ def set_image_type(web_driver):
 
 def set_moderation_off(web_driver):
     try:
-        filter_dropdown = web_driver.find_element_by_class_name("dropdown--safe-search")
+        filter_dropdown = WebDriverWait(web_driver, 15).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "dropdown--safe-search"))
+        )
         filter_dropdown.click()
-        modal_list = WebDriverWait(web_driver, timeout=10).until(
+        modal_list = WebDriverWait(web_driver, timeout=15).until(
             lambda d: web_driver.find_element_by_class_name("modal__list")
         )
         anchor_elements = modal_list.find_elements_by_tag_name("a")
@@ -142,41 +157,77 @@ def set_moderation_off(web_driver):
                 anchor_element.click()
                 print("SafeSearch set to: off")
                 break
+        # This sleep is necessary since the window needs to refresh to update images with the new
+        # changes that come with changing the flag in question
+        time.sleep(2)
+
         return True
     except Exception as ex:
         print(ex)
+
         return False
 
 
 def select_first_image(web_driver):
     try:
-        image_tiles = WebDriverWait(web_driver, timeout=10).until(
-            lambda d: web_driver.find_elements_by_class_name("tile--img")
+        image_tile = WebDriverWait(web_driver, timeout=15).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "tile--img"))
         )
-        image_tiles[0].click()  # clicks on the first image
+        image_tile.click()  # clicks on the first image
+
         return True
     except Exception as ex:
         print(ex)
+
         return False  # unable to select the image
 
 
 def get_selected_image_link(web_driver):
     try:
-        selected_image_desc = WebDriverWait(web_driver, timeout=10).until(
-            lambda d: web_driver.find_element_by_class_name("c-detail__desc"))
-        anchor_tag = selected_image_desc.find_element_by_class_name("js-image-detail-link")
+        selected_image_desc = WebDriverWait(web_driver, timeout=15).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "c-detail__desc"))
+        )
+        # scroll image into view
+        web_driver.execute_script("arguments[0].scrollIntoView", selected_image_desc)
+        anchor_tag = WebDriverWait(selected_image_desc, 15).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "js-image-detail-link")))
         image_href = anchor_tag.get_attribute("href")
+        print(f"href=\"{image_href}\"")
+
         return image_href
     except Exception as ex:
+        print("Unable to fetch image href")
         print(ex)
+
         return ""  # shows that the image was not found
 
 
 def move_to_next_image(web_driver):
     try:
-        next_image = web_driver.find_element_by_class_name("js-detail-next")
+        next_image = WebDriverWait(web_driver, 15).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "js-detail-next"))
+        )
         next_image.click()
-        print("next image ->")
+
+        return True
+    except Exception as ex:
+        print(ex)
+
+        return False
+
+
+def dismiss_add_to_chrome_badge(web_driver):
+    try:
+        badge_link = WebDriverWait(web_driver, 15).until(
+            lambda d: web_driver.find_element(By.CLASS_NAME, "badge-link")
+        )
+
+        if badge_link.is_displayed():
+            badge_link_dismiss = WebDriverWait(web_driver, 15).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "js-badge-link-dismiss"))
+            )
+            badge_link_dismiss.click()
+
         return True
     except Exception as ex:
         print(ex)
@@ -186,34 +237,39 @@ def move_to_next_image(web_driver):
 if __name__ == "__main__":
     print("Running __main__")
     search_for_item(driver, "Test Search")
+
     time.sleep(3)
-    duckbar_static = WebDriverWait(driver, timeout=10).until(
-        lambda d: driver.find_element_by_id("duckbar_static")
-    )
-    metabar = None
-    try:
-        metabar = WebDriverWait(driver, timeout=10).until(
-            lambda d: driver.find_element_by_class_name("metabar__dropdowns")
-        )
-    except Exception as e:
-        print(e)
-        pass
-    # turn_off_search_moderation(driver)
-    # tries to run the whole thing like a graph
-    action_graph = [
+    # set up the configuration to allow for explicit images to be shown
+    config_graph = [
+        dismiss_add_to_chrome_badge,
         select_image_tab,
         set_moderation_off,
         set_image_size,
         set_image_type,
+    ]
+
+    print("setting up image config")
+    for i, func in enumerate(config_graph):
+        print(f"Step: {i}, func: {func.__name__}")
+        if func.__name__ == "get_selected_image_link":
+            link = func(driver)
+        else:
+            success = False
+            while not success:
+                success = func(driver)
+
+    # tries to run the whole thing like a graph
+    action_graph = [
         select_first_image,
         get_selected_image_link,
         move_to_next_image,
     ]
 
-    for func in action_graph:
+    print("fetching images...")
+    for i, func in enumerate(action_graph):
+        print(f"Step: {i}, func: {func.__name__}")
         if func.__name__ == "get_selected_image_link":
             link = func(driver)
-            print(f"gotten link: {link}")
         else:
             success = False
             while not success:
